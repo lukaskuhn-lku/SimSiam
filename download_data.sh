9dfc053fd4e472cb788857024c1f1dbef07f68e7
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2024-08-06 21:27:15 +0200

    update download script

--- download_data.sh
+++ download_data.sh
@@ -10,7 +10,7 @@ mkdir -p $TARGET_DIR
 # Download the zip file using curl
 curl -o /tmp/ISIC_2024_Training_Input.zip $URL
 
-curl -o /data/metadata.csv https://isic-challenge-data.s3.amazonaws.com/2024/ISIC_2024_Training_GroundTruth.csv
+curl -o data/metadata.csv https://isic-challenge-data.s3.amazonaws.com/2024/ISIC_2024_Training_GroundTruth.csv
 
 # Extract the contents of the zip file to the target directory
 unzip -q /tmp/ISIC_2024_Training_Input.zip -d $TARGET_DIR
